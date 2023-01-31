cc45f96a6028416193e6bda9392f7cdf8b7816d5
Jeremy White <jcwhite414@gmail.com>
2023-01-30 20:29:48 -0600

    Edited comments

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,37 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using RockPaperScissors;
+﻿using RockPaperScissors;
 
-Console.WriteLine("Hello, World!");
-//rock
-string rock = @"
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)";
-
-string paper = @"
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)";
-
-string scissors = @"
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)";
-
-//gotta displllay the options of rock paper scissors, and have a scoreboard displaying the results.
-
-
-
-//gotta have a way for the player to pick their chosen sign
+//Requirements/process
+//have to display the options of rock paper scissors, and have a scoreboard displaying the results.
+//have to have a way for the player to pick their chosen sign
 //after the player picks their sign, the computer will pick their sign.
 //There needs to be a list of rules of what beats what. rock/paper/scisors has to have specified rules for each scenario, doesn't equate to a number less than/greater than relationship.
 //possible matchups:
@@ -42,9 +13,7 @@ string scissors = @"
 
 //TODO
 //Create NPC class to hold npc score and to get a random  choice for the NPC
-
 //create player class to hold the player score and hold the choice of the player. Optionally have a highScore associated with that player.
-
 //create a game class that starts the process of looping matches and will quit if the player types quit.
 //ccreate a match class that gathers actual input from the user, gets the npc's choice, and assesses the winner.
 
